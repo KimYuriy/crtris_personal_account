e0a633f26944d42205ca6cb8fbd2c5e7c282e11a
KimYuriy <71920431+KimYuriy@users.noreply.github.com>
2024-01-05 18:06:57 +0300

    fix(NavBar): Comment on option
    Commented option with title 'Отчеты'

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -51,7 +51,7 @@ export default {
             options: [
                 { title: 'Мой профиль', path: '/' },
                 { title: 'Профиль куратора', path: '/curator' },
-                { title: 'Отчеты', path: '/'},
+                // { title: 'Отчеты', path: '/'},
                 // { title: 'Настройки', path: '/' }
             ],
             firstName: "Имя",
